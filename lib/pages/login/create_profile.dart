4ae8ad5ce5f478d2c1fb2354677da743b2dd8287
dychrstn <dy.cavd@gmail.com>
2024-02-15 21:35:39 +0800

    Profile Sign in Fixed

--- lib/pages/login/create_profile.dart
+++ lib/pages/login/create_profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sari/pages/main.dart';
+import 'package:sari/pages/login/profile_signup.dart';
 import 'package:logger/logger.dart';
 
 class CreateProfile extends StatefulWidget {
@@ -12,15 +13,16 @@ class CreateProfile extends StatefulWidget {
 
 class _CreateProfileState extends State<CreateProfile> {
   List<String> profileNames = ['Christian'];
+  final profileController = TextEditingController();
 
   void handleProfileTap(int index) {
     // Handle profile tap logic here (e.g., navigate to profile page)
     Logger().d("Profile $index tapped: ${profileNames[index]}");
   }
 
-  void addNewProfile() {
+  void addNewProfile(String profile) {
     setState(() {
-      profileNames.add('New Profile');
+      profileNames.add(profile);
     });
   }
 
@@ -32,9 +34,12 @@ class _CreateProfileState extends State<CreateProfile> {
         body: Container(
           color: const Color(0xFFB3CDE4),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const SizedBox(
+                height: 60,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -53,49 +58,79 @@ class _CreateProfileState extends State<CreateProfile> {
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 1.0,
-                    mainAxisSpacing: 1.0,
-                  ),
-                  itemCount: profileNames.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                        onTap: () => Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const CreateProfile()),
-                              //MaterialPageRoute(builder: (context) => const Correction()),
-                            ), // Call handleProfileTap
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              width: 93,
-                              height: 93,
-                              child: CircleAvatar(
-                                backgroundColor: Color(0xFF1D3F58),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 3,
-                            ),
-                            Text(profileNames[index])
-                          ],
-                        ));
-                  },
-                ),
-              ),
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: StatefulBuilder(
+                    builder: (context, setState) {
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 1.0,
+                          mainAxisSpacing: 1.0,
+                        ),
+                        itemCount: profileNames.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                              onTap: () => Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const MyApp()),
+                                    //MaterialPageRoute(builder: (context) => const Correction()),
+                                  ), // Call handleProfileTap
+                              child: Column(
+                                children: [
+                                  const SizedBox(
+                                    width: 75,
+                                    height: 75,
+                                    child: CircleAvatar(
+                                      backgroundColor: Color(0xFF1D3F58),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 3,
+                                  ),
+                                  Text(profileNames[index])
+                                ],
+                              ));
+                        },
+                      );
+                    },
+                  )),
               Column(
                 children: [
-                  IconButton(
-                      onPressed: () {
-                        setState(() {
-                          profileNames.add('New Profile');
-                        });
-                      }, icon: const Icon(Icons.person_add)),
+                  Material(
+                    color: Colors.transparent,
+                    child: Center(
+                      child: SizedBox(
+                        height: 75,
+                        width: 75,
+                        child: Ink(
+                          decoration: const ShapeDecoration(
+                            color: Color(0xFF1D3F58),
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.person_add),
+                            color: Colors.white,
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ProfileSignup(
+                                            controller: profileController,
+                                          ))).then((newProfile) {
+                                if (newProfile != null) {
+                                  addNewProfile(newProfile);
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                   const SizedBox(
                     height: 3,
                   ),
--- lib/pages/login/profile_signup.dart
+++ lib/pages/login/profile_signup.dart
@@ -1,10 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logger/logger.dart';
 
-class ProfileSignup extends StatelessWidget {
-  const ProfileSignup({super.key});
+class ProfileSignup extends StatefulWidget {
+  final TextEditingController controller;
+
+  ProfileSignup({super.key, required this.controller});
+
+  @override
+  State<ProfileSignup> createState() => _ProfileSignupState();
+}
+
+class _ProfileSignupState extends State<ProfileSignup> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Profile Signup'),);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: const Color(0xFFB3CDE4),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'lib/assets/Sari-Logo.svg',
+                      width: 131,
+                      height: 93,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: Center(
+                    child: SizedBox(
+                      height: 84,
+                      width: 84,
+                      child: Ink(
+                        decoration: const ShapeDecoration(
+                          color: Color(0xFF1D3F58),
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.person_add),
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: widget.controller,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    keyboardType: TextInputType.name,
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context, widget.controller.text);
+                                widget.controller.text = '';
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFF001B2E),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              child: const Text(
+                                'UPLOAD',
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFEEF3F9),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              child: const Text(
+                                'CANCEL',
+                                style: TextStyle(color: Color(0xFF1D3F58)),
+                              )),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
